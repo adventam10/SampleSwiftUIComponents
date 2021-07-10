0117e07b6dcaef644f842c5165f40b148cb74929
am10 <adventam10@gmail.com>
2021-07-10 18:24:25 +0900

    Add Grids

--- SwiftUIComponents/SwiftUIComponents/Components/LayoutAndPresentation/GridsView.swift
+++ SwiftUIComponents/SwiftUIComponents/Components/LayoutAndPresentation/GridsView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct GridsView: View {
+
+    private let systemImages = [
+        "square.and.arrow.up", "square.and.arrow.up.fill",
+        "square.and.arrow.down", "square.and.arrow.down.fill",
+        "pencil", "pencil.circle",
+        "pencil.circle.fill", "pencil.slash",
+        "square.and.pencil", "scribble",
+        "scribble.variable", "highlighter",
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationTitle("Grids")
+        VStack {
+            let rows: [GridItem] =
+                    Array(repeating: .init(.fixed(44)), count: 2)
+            HeaderView(text: "LazyHGrid")
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: rows, alignment: .top) {
+                    ForEach(systemImages, id: \.self) { name in
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.blue)
+                            VStack {
+                                Text(name)
+                                Image(systemName: name)
+                            }
+                        }
+                    }
+                }
+            }
+
+            let columns: [GridItem] =
+                Array(repeating: .init(.flexible()), count: 2)
+            HeaderView(text: "LazyVGrid")
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(systemImages, id: \.self) { name in
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.green)
+                            VStack {
+                                Text(name)
+                                Image(systemName: name)
+                            }
+                        }
+                    }
+                }
+            }
+        }.navigationTitle("Grids")
     }
 }
 
