858e8ed45cc1f3e48a3ea5ac3bf0867d48aea77e
am10 <adventam10@gmail.com>
2021-07-10 21:00:39 +0900

    Add Hierarchical Views

--- SwiftUIComponents/SwiftUIComponents/Components/LayoutAndPresentation/HierarchicalViewsView.swift
+++ SwiftUIComponents/SwiftUIComponents/Components/LayoutAndPresentation/HierarchicalViewsView.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct HierarchicalViewsView: View {
+
+    struct FileItem: Hashable, Identifiable, CustomStringConvertible {
+        var id: Self { self }
+        var name: String
+        var children: [FileItem]? = nil
+        var description: String {
+            switch children {
+            case nil:
+                return "📄 \(name)"
+            case .some(let children):
+                return children.isEmpty ? "📂 \(name)" : "📁 \(name)"
+            }
+        }
+    }
+
+    let data =
+      FileItem(name: "users", children:
+        [FileItem(name: "user1234", children:
+          [FileItem(name: "Photos", children:
+            [FileItem(name: "photo001.jpg"),
+             FileItem(name: "photo002.jpg")]),
+           FileItem(name: "Movies", children:
+             [FileItem(name: "movie001.mp4")]),
+              FileItem(name: "Documents", children: [])
+          ]),
+         FileItem(name: "newuser", children:
+           [FileItem(name: "Documents", children: [])
+           ])
+        ])
+
+    @State private var expanded = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationTitle("Hierarchical Views")
+        VStack {
+            HeaderView(text: "NavigationView")
+            NavigationView {
+                Text("Select a Note").navigationTitle("Text")
+            }
+
+            HeaderView(text: "OutlineGroup")
+            OutlineGroup(data, children: \.children) { item in
+                Text("\(item.description)")
+            }
+
+            HeaderView(text: "DisclosureGroup")
+            DisclosureGroup("Items", isExpanded: $expanded) {
+                Text("Item 1")
+                Text("Item 2")
+                DisclosureGroup("Sub-items") {
+                    Text("Sub-item 1")
+                }
+            }
+
+            HeaderView(text: "TabView")
+            TabView {
+                Text("The First Tab")
+                    .tabItem {
+                        Image(systemName: "1.square.fill")
+                        Text("First")
+                    }
+                Text("Another Tab")
+                    .tabItem {
+                        Image(systemName: "2.square.fill")
+                        Text("Second")
+                    }
+                Text("The Last Tab")
+                    .tabItem {
+                        Image(systemName: "3.square.fill")
+                        Text("Third")
+                    }
+            }
+        }.navigationTitle("Hierarchical Views")
     }
 }
 
