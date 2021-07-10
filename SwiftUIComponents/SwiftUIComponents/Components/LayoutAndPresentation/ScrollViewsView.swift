1f6977fadc253d5d161e9e63d4b63f653000f738
am10 <adventam10@gmail.com>
2021-07-10 18:37:46 +0900

    Add Scroll Views

--- SwiftUIComponents/SwiftUIComponents/Components/LayoutAndPresentation/ScrollViewsView.swift
+++ SwiftUIComponents/SwiftUIComponents/Components/LayoutAndPresentation/ScrollViewsView.swift
@@ -7,11 +7,51 @@
 
 import SwiftUI
 
+
 struct ScrollViewsView: View {
+
+    @Namespace var topID
+    @Namespace var bottomID
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationTitle("Scroll Views")
+        VStack {
+            HeaderView(text: "ScrollView")
+            ScrollView {
+                VStack {
+                    ForEach(0..<100) {
+                        Text("Row \($0)")
+                    }
+                }
+            }
+
+            HeaderView(text: "ScrollViewReader")
+            ScrollViewReader { proxy in
+                ScrollView {
+                    Button("Scroll to Bottom") {
+                        withAnimation {
+                            proxy.scrollTo(bottomID)
+                        }
+                    }
+                    .id(topID)
+
+                    VStack {
+                        ForEach(0..<100) {
+                            Text("Row \($0)")
+                        }
+                    }
+
+                    Button("Top") {
+                        withAnimation {
+                            proxy.scrollTo(topID)
+                        }
+                    }
+                    .id(bottomID)
+                }
+            }
+        }.navigationTitle("Scroll Views")
     }
 }
+
 
 struct ScrollViewsView_Previews: PreviewProvider {
     static var previews: some View {
