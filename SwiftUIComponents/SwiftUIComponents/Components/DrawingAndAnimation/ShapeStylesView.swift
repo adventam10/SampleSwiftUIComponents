ade5847c63da1c96f942e0c1ffedd662330354df
am10 <adventam10@gmail.com>
2021-07-14 01:54:21 +0900

    Add Shape Styles

--- SwiftUIComponents/SwiftUIComponents/Components/DrawingAndAnimation/ShapeStylesView.swift
+++ SwiftUIComponents/SwiftUIComponents/Components/DrawingAndAnimation/ShapeStylesView.swift
@@ -11,8 +11,41 @@ struct ShapeStylesView: View {
 
     var body: some View {
         VStack {
-            HeaderView(text: "Text")
-            Text("Hello, world!")
+            Group {
+                HeaderView(text: "Color")
+                Color(.systemRed)
+                    .frame(width: 44, height: 44)
+
+                // Material（iOS15以上）
+
+                HeaderView(text: "ImagePaint")
+                Rectangle()
+                    .fill(ImagePaint(image: Image("image"), scale: 0.1))
+                    .frame(width: 44, height: 44)
+            }
+
+            Group {
+                HeaderView(text: "AngularGradient")
+                AngularGradient(gradient: Gradient(colors: [.red, .black]),
+                                center: .center, angle: .degrees(45))
+                    .frame(width: 44, height: 44)
+
+                // EllipticalGradient（iOS15以上）
+
+                HeaderView(text: "LinearGradient")
+                LinearGradient(gradient: Gradient(colors: [.green, .black]),
+                               startPoint: .leading,
+                               endPoint: .trailing)
+                    .frame(width: 44, height: 44)
+
+                HeaderView(text: "RadialGradient")
+                RadialGradient(gradient: Gradient(colors: [.blue, .black]),
+                               center: .center,
+                               startRadius: 0,
+                               endRadius: 22)
+                    .frame(width: 44, height: 44)
+
+            }
         }.navigationTitle("Shape Styles")
     }
 }
