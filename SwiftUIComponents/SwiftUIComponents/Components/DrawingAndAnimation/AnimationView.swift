1ae6269a3e9ac49f023cee21f031efff46d376a1
am10 <adventam10@gmail.com>
2021-07-18 17:51:36 +0900

    Add Animation

--- SwiftUIComponents/SwiftUIComponents/Components/DrawingAndAnimation/AnimationView.swift
+++ SwiftUIComponents/SwiftUIComponents/Components/DrawingAndAnimation/AnimationView.swift
@@ -9,10 +9,119 @@ import SwiftUI
 
 struct AnimationView: View {
 
+    @State private var isHidden = false
+    @State private var isTransition = false
+
     var body: some View {
         VStack {
-            HeaderView(text: "Text")
-            Text("Hello, world!")
+            HeaderView(text: "Animation")
+            VStack {
+                HStack {
+                    VStack {
+                        SubHeaderView(text: "default")
+                        Circle()
+                            .fill(Color.purple)
+                            .frame(width: 44, height: 44)
+                            .opacity(isHidden ? 0.0 : 1.0)
+                            .animation(.default)
+                    }
+                    VStack {
+                        SubHeaderView(text: "easeIn")
+                        Circle()
+                            .fill(Color.purple)
+                            .frame(width: 44, height: 44)
+                            .opacity(isHidden ? 0.0 : 1.0)
+                            .animation(.easeIn(duration: 1.5))
+                    }
+                    VStack {
+                        SubHeaderView(text: "easeInOut")
+                        Circle()
+                            .fill(Color.purple)
+                            .frame(width: 44, height: 44)
+                            .opacity(isHidden ? 0.0 : 1.0)
+                            .animation(.easeInOut(duration: 1.5))
+                    }
+                    VStack {
+                        SubHeaderView(text: "easeOut")
+                        Circle()
+                            .fill(Color.purple)
+                            .frame(width: 44, height: 44)
+                            .opacity(isHidden ? 0.0 : 1.0)
+                            .animation(.easeOut(duration: 1.5))
+                    }
+                    VStack {
+                        SubHeaderView(text: "linear")
+                        Circle()
+                        .fill(Color.purple)
+                        .frame(width: 44, height: 44)
+                        .opacity(isHidden ? 0.0 : 1.0)
+                        .animation(.linear(duration: 1.5))
+                    }
+                }
+
+                Button("Animation!") {
+                    self.isHidden.toggle()
+                }
+            }
+
+            // わからん
+            HeaderView(text: "AnimatablePair")
+            Text("🤷‍♂️")
+
+            // わからん
+            HeaderView(text: "EmptyAnimatableData")
+            Text("🤷‍♂️")
+            // EmptyAnimatableData()
+
+
+            HeaderView(text: "AnyTransition")
+            VStack {
+                Button("Transition!") {
+                    withAnimation {
+                        isTransition.toggle()
+                    }
+                }
+                HStack {
+                    VStack {
+                        SubHeaderView(text: "identity")
+                        if isTransition {
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: 44, height: 44)
+                                .transition(AnyTransition.identity)
+                        }
+                    }
+                    VStack {
+                        SubHeaderView(text: "opacity")
+                        if isTransition {
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: 44, height: 44)
+                                .transition(AnyTransition.opacity)
+                        }
+                    }
+                    VStack {
+                        SubHeaderView(text: "scale")
+                        if isTransition {
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: 44, height: 44)
+                                .transition(AnyTransition.scale)
+                        }
+                    }
+                    VStack {
+                        SubHeaderView(text: "slide")
+                        if isTransition {
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: 44, height: 44)
+                                .transition(AnyTransition.slide)
+                        }
+                    }
+                }
+            }
+
+            Spacer()
         }.navigationTitle("Animation")
     }
 }
